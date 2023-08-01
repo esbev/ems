7b3eabb3bb010ee2a1bd43edcb2f6ed2c558aa9b
Eric <esbev@hotmail.com>
2023-08-01 16:41:29 -0500

    working on switch case function choices

--- index.js
+++ index.js
@@ -12,76 +12,102 @@ const db = mysql.createConnection({
 
 let exit = false;
 
-async function init() {
-  while (exit) {//menu choice goes back to main at end of each function of choices until quit/exit is true
-    await inquirer
-      .prompt(mainMenu)
-      .then((input) => {
-        switch (input.choice) {
-          case "mainMenu":
-            break;
-          case "addDepartment":
-            break;
-          case "addRole":
-            break;
-          case "addEmployee":
-            break;
-          case "updateEmployee":
-            break;
-          case "quit":
-            break;
-          default:
-            exit = true;
-            break;
-        }
-      });
+async function init(menuChoice) {
+  while (!exit) {//menu choice goes back to main at end of each function of choices until quit/exit is true
+    switch (menuChoice) {
+      case "viewDepartments":
+        tableChoice = "department";
+        printQuery(tableChoice);
+        break;
+      case "viewRoles":
+        tableChoice = "roles";
+        printQuery(tableChoice);
+        break;
+      case "viewEmployees":
+        tableChoice = "employees";
+        printQuery(tableChoice);
+        break;
+      case "addDepartment":
+        addNewDepartment();
+        break;
+      case "addRole":
+        break;
+      case "addEmployee":
+        break;
+      case "updateEmployee":
+        break;
+      case "quit":
+        exit = true;
+        break;
+      default:
+        //mainMenu
+        break;
+    }
   }
-  console.log("thank you for using EMS!")
+  console.log("Thank you for using EMS!")
 };
 
-let mainMenu = [
-  {
-    type: "list",
-    name: "choice",
-    message: "What would you like to do?",
-    choices: [
-      { name: "View all departments", value: "viewDepartments" },
-      { name: "View all roles", value: "viewRoles" },
-      { name: "View all employees", value: "viewEmployees" },
-      { name: "Add a department", value: "addDepartment" },
-      { name: "Add a role", value: "addRole" },
-      { name: "Add an employee", value: "addEmployee" },
-      { name: "Update an employee", value: "updateEmployee" },
-      { name: "Quit", value: "quit" }
-    ],
-  }
-];
+async function mainMenu() {
 
-let addDeptMenu = [
-  {
-    type: "input",
-    name: "department",
-    message: "What department would you like to add?",
-  }
-];
+  let mainMenu = [
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
+        { name: "View all departments", value: "viewDepartments" },
+        { name: "View all roles", value: "viewRoles" },
+        { name: "View all employees", value: "viewEmployees" },
+        { name: "Add a department", value: "addDepartment" },
+        { name: "Add a role", value: "addRole" },
+        { name: "Add an employee", value: "addEmployee" },
+        { name: "Update an employee role", value: "updateEmployee" },
+        { name: "Quit", value: "quit" }
+      ],
+    }
+  ];
 
-let addRoleMenu = [
-  {
-    type: "input",
-    name: "role",
-    message: "What Role would you like to add?",
-  },
-  {
-    input: "list",
-    name: "salary",
-    message: "What is the salary for this role?",
-  },
-  {
-    input: "list",
-    name: "deptId",
-    message: "What department does this role belong to?",
-  },
-];
+}
+
+async function addNewDepartment() {
+
+  let addDept = [
+    {
+      type: "input",
+      name: "newDepartment",
+      message: "Type the department you would like to add.",
+    }
+  ];
+  await inquirer.prompt(addDept)
+  .then((input) => {
+    db.query(`INSERT INTO department(dept_name) VALUE(${input.newDepartment})`);
+    console.log("Department Added");
+    return "mainMenu";
+  })
+
+};
+
+//todo: finish
+async function addNewRole() {
+
+  let addRole = [
+    {
+      type: "input",
+      name: "newRole",
+      message: "Type the role you would like to add.",
+    },
+    {
+      input: "list",
+      name: "salary",
+      message: "What is the salary for this role?",
+    },
+    {
+      input: "list",
+      name: "deptId",
+      message: "Type the ID of the department for this role.",
+    },
+  ];
+}
 
 let addEmpMenu = [
   {
@@ -97,7 +123,20 @@ let addEmpMenu = [
   {
     input: "list",
     name: "empRole",
-    message: "What is this employee's role?",
+    message: "Type the ID for the role for this employee.",
+  },
+];
+
+let updateEmpRoleMenu = [
+  {
+    type: "input",
+    name: "employeeId",
+    message: "Enter employee ID to update",
+  },
+  {
+    type: "input",
+    name: "roleChoice",
+    message: "Type the ID for the new role.",
   },
 ];
 
@@ -123,10 +162,9 @@ function tableSelection() {
 
 
 function printQuery(tableChoice) {
-  // db.query('SELECT * FROM '+ tableChoice, function (err, results) {
-  //   console.log(results);
-  // });
-  console.log(tableChoice);
+  db.query(`SELECT * FROM ${tableChoice}`, function (err, results) {
+    console.log(results);
+  });
 }
 
 
